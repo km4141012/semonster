29d4c9cfb8ce6d5ff0ccc568ec91006d8ca04309
Yoshimura Kouki <m1m02630148@oit.ac.jp>
2026-06-22 14:41:25 +0900

    mainをマージ

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
+    System.out.println("Show a new monster!");
     Player player = new Player();
     player.drawMonsters();
     player.showDeck();
+    System.out.println(player.toString());
   }
 }
