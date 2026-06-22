226addb2faca930eb1b0dbb232a55a9049a0a74d
Yoshimura Kouki <m1m02630148@oit.ac.jp>
2026-06-22 14:47:08 +0900

    Main変更

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster GAME");
+    System.out.println("SEMONSTER GAME");
     System.out.println("Show a new monster!");
-    Player player = new Player();
-    player.drawMonsters();
-    player.showDeck();
-    System.out.println(player.toString());
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
   }
 }
