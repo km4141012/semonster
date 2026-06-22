b3ffe1c97685fa9958ab1d4fe353c9eaad385055
Yoshimura Kouki <m1m02630148@oit.ac.jp>
2026-06-22 13:44:00 +0900

    Main　機能追加

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    System.out.println("SEMonster GAME");
+    System.out.println("Show a new monster!");
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
+    System.out.println(player.toString());
   }
 }
